6016c3b7b0d0ee557c4e50f37c10fb2da87c4bfb
freshjy <freshjeongyeop@gmail.com>
2020-12-30 16:27:36 +0900

    Spring MVC 연습

--- hellospring03/src/main/java/com/markany/hellospring03/controller/BoardController.java
+++ hellospring03/src/main/java/com/markany/hellospring03/controller/BoardController.java
@@ -1,5 +1,37 @@
 package com.markany.hellospring03.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 
+ * @author bit
+ * 
+ * @RequestMapping 예제
+ * Method(Handler) 단독 매핑
+ * 
+ */
+
+@Controller
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController:write()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view")
+	public String view(int no) {
+		return "BoardController:write("+ no + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view2/{no}")
+	public String view2(@PathVariable("no") int boardNno) {
+		return "BoardController:write("+ boardNno + ")";
+	}
 }
