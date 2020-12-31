d286419b782ae90656a0880d2ce20bd015823d28
freshjy <freshjeongyeop@gmail.com>
2020-12-31 11:39:17 +0900

    emaillist03 #2

--- emaillist03/src/main/java/com/markany/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/markany/emaillist/controller/EmaillistController.java
@@ -1,10 +1,15 @@
 package com.markany.emaillist.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.markany.emaillist.repository.EmaillistRepository;
+import com.markany.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
@@ -13,8 +18,27 @@ public class EmaillistController {
 	private EmaillistRepository emaillistRepository;
 	
 	@RequestMapping("")
-	public String index() {
-		System.out.println(emaillistRepository);
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
 		return "/WEB-INF/views/index.jsp";
 	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "/WEB-INF/views/add.jsp";
+	}
+
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
+	}
 }
+
+
+
+
+
+
+
