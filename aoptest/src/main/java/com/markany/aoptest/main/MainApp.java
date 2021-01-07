3b5ad13dbce7a0061ffccaea55876f7dfdf6222e
freshjy <freshjeongyeop@gmail.com>
2021-01-07 10:55:41 +0900

    aoptest 추가

---
+++ aoptest/src/main/java/com/markany/aoptest/main/MainApp.java
@@ -0,0 +1,22 @@
+package com.markany.aoptest.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.markany.aoptest.service.ProductService;
+import com.markany.aoptest.vo.ProductVo;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		ProductService ps = ac.getBean(ProductService.class);
+//		ProductService ps = new ProductService();
+		ProductVo vo = ps.find("TV");
+		System.out.println(vo);
+
+		((ConfigurableApplicationContext)ac).close();
+	}
+
+}
