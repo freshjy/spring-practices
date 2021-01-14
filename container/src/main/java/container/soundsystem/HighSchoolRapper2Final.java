9733e35c704e2d36e5728f63b7e926a353d51934
freshjy <freshjeongyeop@gmail.com>
2021-01-14 11:59:50 +0900

    soundsystem #3

--- container/src/main/java/container/soundsystem/HighSchoolRapper2Final.java
+++ container/src/main/java/container/soundsystem/HighSchoolRapper2Final.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HighSchoolRapper2Final implements CompactDisc {
-
 	private String title = "붕붕";
 	private String artist = "김하온";
 	
